ee49ab70bacde80bd4a7cf44a04339e850a8877f
Jonathan Cipriano <cipriano.jonathan@gmail.com>
2013-12-11 12:01:40 -0800

    Adding exension.

---
+++ extension/ChromeExt/javascripts/main.js
@@ -0,0 +1,113 @@
+(function() {
+
+  var cc = {};
+
+  cc.config = {
+    cursePoolTotal: 10
+  };
+
+  // Initializes UI
+  cc.init = function() {
+
+    if(chrome){
+      chrome.storage.local.get('firstUse', function(obj) {
+        if(!obj.firstUse) {
+          chrome.storage.local.set({'firstUse': true}, function() { });
+          chrome.storage.local.set({'cursingEnabled': true}, function() {
+            cc.initComplete();
+          });
+
+          return;
+        }
+
+        cc.initComplete();
+      });
+    }
+  };
+
+  cc.initComplete = function() {
+    cc.loadComplete();
+  };
+
+  cc.insertScriptsAndCSS = function() {
+    if(!TweenLite) {
+
+    }
+  };
+
+  cc.loadComplete = function() {
+
+    document.onclick = function(e) {
+      if(chrome){
+        chrome.storage.local.get('cursingEnabled', function(obj) {
+          console.log('cursingEnabled', obj.cursingEnabled);
+          if(obj.cursingEnabled) {
+            cc.onClick(e);
+          }
+        });
+
+        return;
+      }
+
+      cc.onClick(e);
+    };
+  }
+
+  cc.onClick = function(e) {
+
+    var curseId = "cc-curse-view-" + Math.floor((Math.random() * 1000)),
+    data = {
+      id: curseId,
+      src: cc.getRandomCurse(),
+      position: {
+        top: e.pageY ? e.pageY : e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,
+        left: e.pageX ? e.pageX : e.clientY + document.body.scrollTop + document.documentElement.scrollTop
+      }
+    };
+
+    var el = cc.addCurseToDom(data);
+
+    TweenLite.to(el, 0.15, {top: data.position.top, opacity: 1, ease: Quad.easeOut, onComplete: function() {
+      TweenLite.to(el, 0.5, {delay: 1, top: data.position.top - 10, autoAlpha: 0, ease: Quad.easeIn, onComplete: function() {
+        document.body.removeChild(el);
+      }});
+    }});
+
+    if(chrome) {
+      chrome.storage.local.get('totalCurses', function(obj) {
+        console.log(obj);
+        obj.totalCurses = obj.totalCurses ? obj.totalCurses : 0;
+        chrome.storage.local.set({'totalCurses': obj.totalCurses + 1}, function() { });
+      });
+    }
+  };
+
+  cc.getRandomCurse = function() {
+
+    var id = Math.floor(Math.random() * cc.config.cursePoolTotal),
+    url = 'images/curse_' + id + '.png';
+    
+    if(chrome) {
+      url = chrome.extension.getURL(url);
+    }
+    
+    return url;
+  };
+
+  cc.addCurseToDom = function(data) {
+
+    var el = document.createElement('div');   
+    el.id = data.id;
+    el.className = 'cc-curse-view';
+    el.style.background = "url('" + data.src + "')";
+    el.style.top = data.position.top + 10 + 'px';
+    el.style.left = data.position.left + 'px';
+
+    document.body.appendChild(el);   
+
+    return el;
+  };
+
+  cc.init();
+
+})();
